56ee9cf16a3f17b2d4c4ffd64b52f89f04962424
Abhay Pratap Yadav <abhaypratapyadav340@gmail.com>
2022-04-28 23:47:28 +0530

    AudioIntegration

--- To The Sky And Beyond/Assets/Scripts/MainMenu.cs
+++ To The Sky And Beyond/Assets/Scripts/MainMenu.cs
@@ -8,11 +8,13 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        FindObjectOfType<AudioManager>().Play("menuClick");
     }
 
     public void Exit()
     {
         Debug.Log("Exit");
         Application.Quit();
+        FindObjectOfType<AudioManager>().Play("menuClick");
     }
 }

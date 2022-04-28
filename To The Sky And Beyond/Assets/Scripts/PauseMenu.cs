de5154283e0e7cd1cdb4b07cf2061b158f91d34b
Abhay Pratap Yadav <abhaypratapyadav340@gmail.com>
2022-04-28 23:19:26 +0530

    ProjectUpload

---
+++ To The Sky And Beyond/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;  //To Check if game is paused
+
+    public GameObject pauseMenuUI; 
+
+    public void PauseButton()  //To give the pause button power to pause
+    {
+        GameIsPaused = true;
+    }
+
+    private void Update()  //Logic to pause or resume
+    {
+        if (GameIsPaused == true)
+        {
+            Pause();
+        }
+        else
+        {
+            Resume();
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exit");
+    }
+}
---
+++ To The Sky And Beyond/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float speed = 15f;
+
+    public float mapWidth = 5f;
+
+    public float sideWaysForce = 10f;
+    public float sideWaysForce2 = -10f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+
+        Vector2 newPosition = rb.position + Vector2.right * x;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);      
+
+        rb.MovePosition(newPosition);
+    }
+
+    public void Left()
+    {
+        rb.AddForce(transform.right * sideWaysForce2, ForceMode2D.Force);
+    }
+
+    public void Right()
+    {
+
+        rb.AddForce(transform.right * sideWaysForce, ForceMode2D.Force);
+        
+    }
+}
